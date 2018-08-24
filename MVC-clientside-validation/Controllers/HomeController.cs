95a286eb15b47bd8bac6a84a1f62634bb03b98cd
user <user@PC>
2018-08-24 23:34:41 +0600

    Add project files.

---
+++ MVC-clientside-validation/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_clientside_validation.Models;
+
+namespace MVC_clientside_validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CheckInvoiceNumber()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckInvoiceNumber(InvoiceNumber invoiceNumber)
+        {
+            ViewBag.Message = "Ну вот и всё , всё прошло успешно. Данное поле валидно";
+            return View();
+        }
+
+    }
+}
---
+++ MVC-clientside-validation/Models/InvoiceNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace MVC_clientside_validation.Models
+{
+    public class InvoiceNumber
+    {
+        [WebDisplayName("Номер накладной")]
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
+        [StringLength(36, ErrorMessage = "Поле не должно превышать 36 символов")]
+
+        [RegularExpression(@"^[A-Z0-9]\w*[^A-Za-z]$", ErrorMessage = "Номер должен начинаться только с числа или заглавной буквы, и только!\n" +
+                                                                     "Последний символ не должен быть буквой")]
+        public string Invoice { get; set; }
+
+    }
+}
